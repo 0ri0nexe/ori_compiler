be8c6af6951017b105015ff36c3efcb5493c9ab1
0ri0nexe <enzo.mongin10@gmail.com>
2024-01-09 21:01:30 +0100

    removed tests and extended the features ambition

--- tests/commands.ps1
+++
@@ -1,6 +0,0 @@
-rm .\test.exe
-cd F:\programmation\c\C++\projets\ori_compiler\tests\
-nasm.exe -f win64 test.asm -o test.obj
-golink.exe test.obj /entry main /console kernel32.dll
-.\test.exe
-echo $LastExitCode
--- tests/test.asm
+++
@@ -1,9 +0,0 @@
-bits 64
-
-; return 6
-extern ExitProcess
-
-global main
-main:
-	mov rcx, 6
-	call ExitProcess
